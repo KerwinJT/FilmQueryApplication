ddc9a4d44be1748b723b8344ff48129926f1bf01
KerwinJT <kerwinjt@gmail.com>
2020-03-06 16:43:39 -0700

    initial commit

---
+++ src/com/skilldistillery/filmquery/entities/Film.java
@@ -0,0 +1,15 @@
+package com.skilldistillery.filmquery.entities;
+
+public class Film {
+	private int id;
+	private String title;
+	private String description;
+	private Integer year;
+	private int languageID;
+	private int rentalDuration;
+	private double rentalRate;
+	private Integer length;
+	private double replacementCost;
+	private String rating;
+	private String specialFeatures;
+}
